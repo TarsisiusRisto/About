014243d10f4cb066d65c9502333d9f742970dccc
TarsisiusRisto <risto.tarsisius@gmail.com>
2025-03-03 11:19:17 +0700

    add to SQLLite

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Arr;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
@@ -12,46 +13,12 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => [
-        [
-            'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Tarsisius Risto Ardianto',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. At quas non fugit dicta, voluptatum minus! Sed tempore aperiam sunt temporibus nostrum. Adipisci optio tenetur fuga, aut accusantium praesentium quas nulla.'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Tarsisius Risto Ardianto',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. At quas non fugit dicta, voluptatum minus! Sed tempore aperiam sunt temporibus nostrum. Adipisci optio tenetur fuga, aut accusantium praesentium quas nulla.'
-        ]
-    ]]);
+    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
 });
 
-Route::get('/posts/{slug}', function($slug){
-    $posts = [
-        [
-            'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Tarsisius Risto Ardianto',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. At quas non fugit dicta, voluptatum minus! Sed tempore aperiam sunt temporibus nostrum. Adipisci optio tenetur fuga, aut accusantium praesentium quas nulla.'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Tarsisius Risto Ardianto',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. At quas non fugit dicta, voluptatum minus! Sed tempore aperiam sunt temporibus nostrum. Adipisci optio tenetur fuga, aut accusantium praesentium quas nulla.'
-        ]
-        ];
-        $post = Arr::first( $posts, function($post) use ($slug) {
-            return $post['slug'] == $slug;
-        });
-        return view('post', [ 'title' => 'Single Post', 'post' => $post ]);
-        // dd($post); // check
+Route::get('/posts/{post:slug}', function( Post $post){
+    return view('post', [ 'title' => 'Single Post', 'post' => $post ]);
+    // dd($post); // check
 });
 
 Route::get('/contact', function () {
